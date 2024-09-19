6a60280c8cb1e81b284239c4551098e4f652aacd
Fırat Demir <firat.23d@gmail.com>
2024-09-19 19:21:08 +0300

    javascript modules update

---
+++ week6/WTC5/modules.js
@@ -0,0 +1,15 @@
+const name = 'Fırat';
+const surname = 'Demir';
+export {name, surname};
+
+export const age = 28;
+export function sayhi(){
+    console.log('Merhaba', name);
+}
+const message = () => {
+    const name = "Jesse";
+    const age = 40;
+    return name + ' is ' + age + 'years old.';
+    };
+    
+    export default message;
