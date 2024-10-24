fd957a0bdc86c82277f6adfe58deba8b0fdd5e55
Fırat Demir <firat.23d@gmail.com>
2024-10-24 12:45:51 +0300

    files updated

---
+++ week11/puzzlegame/src/data.js
@@ -0,0 +1,6 @@
+export const data =[
+    {question: 'Bir hayvan adı 😺 ?', answer: 'Kedi'},
+    {question: 'Bir köpek cinsi  🐕 ?', answer: 'Pitbull'},
+    {question: 'Bir şehir adı 🏙️ ?', answer: 'Ankara'},
+    {question: 'Bir Balık adı 🐟 ?', answer: 'Japon'}
+]
